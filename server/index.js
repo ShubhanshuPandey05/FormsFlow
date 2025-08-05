89cf50e12cc9fd2581113c3dd0f6faa5342a75e5
Shubhanshu Pandey <webdevshubhanshu@gmail.com>
2025-08-05 15:50:45 +0530

    adding the ping URL

--- server/index.js
+++ server/index.js
@@ -119,6 +119,17 @@ function pingServer() {
       .catch(error => {
           console.error('Error pinging the server:', error);
       });
+  fetch('https://instatodrive.onrender.com/trigger')
+      .then(response => {
+          if (response.ok) {
+              // console.log('Server is reachable');
+          } else {
+              console.error('Server responded with an error:', response.status);
+          }
+      })
+      .catch(error => {
+          console.error('Error pinging the server:', error);
+      });
 }
 
 // Ping the server every 30 seconds
